df8efb92e0dccb779e3f7b82259699d5354013d4
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 21:43:35 +0100

    feat: v24 bot author verification

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v23 test
+// Demo Service A - v24 test
 function handler(req, res) {
-  console.log("v23 test in Service A");
-  return { status: 200, message: "Hello from Service A v23-bot-author-verification" };
+  console.log("v24 test in Service A");
+  return { status: 200, message: "Hello from Service A v24-bot-author-verification" };
 }
 
 module.exports = { handler };
