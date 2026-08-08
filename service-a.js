13b98fb80bf54b782fad4db67c53a895e32c63b8
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 19:40:35 +0100

    feat: v14 clean dedup test

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v13 test
+// Demo Service A - v14 test
 function handler(req, res) {
-  console.log("v13 test in Service A");
-  return { status: 200, message: "Hello from Service A v13-final-verification" };
+  console.log("v14 test in Service A");
+  return { status: 200, message: "Hello from Service A v14-clean-dedup-test" };
 }
 
 module.exports = { handler };
