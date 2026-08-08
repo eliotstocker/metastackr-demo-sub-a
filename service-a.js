d6952a1ab3eded4af5372135871760e9fe32725a
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 21:39:58 +0100

    feat: v21 metastackr bot author test

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v20 test
+// Demo Service A - v21 test
 function handler(req, res) {
-  console.log("v20 test in Service A");
-  return { status: 200, message: "Hello from Service A v20-clean-body-test" };
+  console.log("v21 test in Service A");
+  return { status: 200, message: "Hello from Service A v21-metastackr-bot-author-test" };
 }
 
 module.exports = { handler };
