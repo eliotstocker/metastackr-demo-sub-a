c7e2cd5d201365a18790ddd6a2aeb7414e137442
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 19:32:40 +0100

    feat: v5 test synchronous lambda

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v4 live lambda test
+// Demo Service A - v5 test
 function handler(req, res) {
-  console.log("v4 live lambda test in Service A");
-  return { status: 200, message: "Hello from Service A v4-live-lambda-test" };
+  console.log("v5 test in Service A");
+  return { status: 200, message: "Hello from Service A v5-synchronous-lambda" };
 }
 
 module.exports = { handler };
