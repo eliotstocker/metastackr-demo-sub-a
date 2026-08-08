5cc3474d01bf842fdc93a39f2baaa7323589353d
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 21:50:09 +0100

    feat: v25 db token cache test

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v24 test
+// Demo Service A - v25 test
 function handler(req, res) {
-  console.log("v24 test in Service A");
-  return { status: 200, message: "Hello from Service A v24-bot-author-verification" };
+  console.log("v25 test in Service A");
+  return { status: 200, message: "Hello from Service A v25-db-token-cache-test" };
 }
 
 module.exports = { handler };
