4139611f2dcdb06a61fe12ef88524341c19d3176
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 18:58:53 +0100

    update: service-a v2

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
 // Demo Service A
 function handler(req, res) {
-  console.log("Processing request in Service A");
-  return { status: 200, message: "Hello from Service A" };
+  console.log("Processing request in Service A - v2");
+  return { status: 200, message: "Hello from Service A v2" };
 }
 
 module.exports = { handler };
