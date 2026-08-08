ebc9ae45a009dda8f9997fb46cdc846824e41b93
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 21:35:01 +0100

    feat: v18 pr body merge test

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v17 test
+// Demo Service A - v18 test
 function handler(req, res) {
-  console.log("v17 test in Service A");
-  return { status: 200, message: "Hello from Service A v17-single-comment-and-status-test" };
+  console.log("v18 test in Service A");
+  return { status: 200, message: "Hello from Service A v18-pr-body-merge-test" };
 }
 
 module.exports = { handler };
