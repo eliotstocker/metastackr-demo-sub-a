322c53ce86675f6eb216a1d9be44f21c18b2af31
Eliot Stocker <eliot@piratemedia.tv>
2026-08-08 21:42:52 +0100

    feat: v23 bot author verification

--- service-a.js
+++ service-a.js
@@ -1,7 +1,7 @@
-// Demo Service A - v22 test
+// Demo Service A - v23 test
 function handler(req, res) {
-  console.log("v22 test in Service A");
-  return { status: 200, message: "Hello from Service A v22-metastackr-bot-sticky-comment-test" };
+  console.log("v23 test in Service A");
+  return { status: 200, message: "Hello from Service A v23-bot-author-verification" };
 }
 
 module.exports = { handler };
